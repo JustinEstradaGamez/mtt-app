d2f195648ee69caee02446562d85beba5a65f92d
JustinEstradaGamez <mulchthistrimthat@gmail.com>
2025-10-19 20:35:01 -0400

    Update next.config.ts
    Convert next.config.ts to JS format for Next 14

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
-    // ✅ Allow deploy even with type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ✅ Allow deploy even with lint errors
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
